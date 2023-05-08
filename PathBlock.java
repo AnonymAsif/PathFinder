cc48c3965a48aaa4cab607ae50055b612213937e
Asif Rahman <arahm1@ocdsb.ca>
2023-05-08 10:04:00 -0400

    created project

---
+++ PathBlock.java
@@ -0,0 +1,12 @@
+
+/**
+ * Block class for a maze solver or pathfinder
+ * Each block can be in a variety of states
+ *
+ * @author Asif Rahman
+ * @version 08/05/2023
+ */
+public class PathBlock
+{
+    
+}

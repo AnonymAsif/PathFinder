ae989ed311902ebba6e6e4cb10df881cad34b130
Asif Rahman <arahm1@ocdsb.ca>
2023-06-01 09:32:25 -0400

    I literally added a class and that's it

---
+++ MazeFileHandler.java
@@ -0,0 +1,21 @@
+
+/**
+ * Reads and writes Mazes to files
+ * Mazes include trails, trees, location of the ranger and cabin
+ * 
+ *
+ * @author Asif Rahman
+ * @version 01/06/2023
+ */
+public class MazeFileHandler
+{
+    // File where the maze is written to
+    private final String MAZE_FILE_PATH = "maze.txt";
+    
+    // Maze to parse, read and write from/to file
+    private PathBlock[] maze;
+    
+    public void readMaze() {
+        
+    }
+}
